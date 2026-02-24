c27ac941fb5f451d4e3f8d33051823e3f33b19be
faradizalovy <lovyfaradiza@gmail.com>
2026-02-24 09:54:27 +0700

    Modifikasi MahasiswaDemo 3.3.1

---
+++ ModifikasiMahasiswaDemo09.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class ModifikasiMahasiswaDemo09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa09[] arrayOfMahasiswa = new Mahasiswa09[3];
+        String dummy;
+        
+        for (int i = 0; i < arrayOfMahasiswa.length; i++) {
+            arrayOfMahasiswa[i] = new Mahasiswa09();
+
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : ");
+            arrayOfMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama  : ");
+            arrayOfMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas : ");
+            arrayOfMahasiswa[i].kelas = sc.nextLine();
+            System.out.print("IPK   : ");
+            dummy = sc.nextLine();
+            arrayOfMahasiswa[i].ipk = Float.parseFloat(dummy);
+            System.out.println("--------------------------------------");
+        }
+        for (int i = 0; i <3; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            System.out.println("NIM   : " + arrayOfMahasiswa[i].nim);
+            System.out.println("Nama  : " + arrayOfMahasiswa[i].nama);
+            System.out.println("Kelas : " + arrayOfMahasiswa[i].kelas);
+            System.out.println("IPK   : " + arrayOfMahasiswa[i].ipk);
+            System.out.println("--------------------------------------");
+        }
+        for (int i = 0; i < arrayOfMahasiswa.length; i++) {
+            arrayOfMahasiswa[i].cetakInfo();
+        }
+        sc.close();
+    }
+}
